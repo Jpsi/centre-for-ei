236b81f61323562bcb340181e1dcbc866b5c3638
Philip Fol <philip.fol08@gmail.com>
2020-02-14 08:26:09 +0000

    Edit homepage text

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -9,13 +9,12 @@ const App = props => {
 <p>We believe that a basic practical knowledge of Emotional Intelligence (EI) is essential for individual happiness, outstanding teamwork, and fundamentally, a fairer society.</p>
 <p>Our goals are to:
 <ul>
-  <li>organise accessible classes on EI in London led by professionals who are passionate about understanding human emotions</li>
-  <li>provide training workshops for businesses that want to improve the EI skills of their employees</li>
+  <li>organise practical and accessible classes on EI led by thought leaders who are passionate about understanding human emotions</li>
   <li>build a tested, structured, and teachable curriculum of EI technique</li>
-  <li>connect with people/organisations who see the global importance and value of EI education</li>
+  <li>connect with people/organisations who recognise the global importance and value of EI education</li>
 </ul></p>
-<p>We are looking for like-minded people. If you are one of them, get in touch at <a href="mailto:connect@centreforei.com">connect@centreforei.com</a></p>
-<p>London, Feburary 2020</p>
+<p>We are looking for people who share this vision. If you are one of them, feel free to get in touch at <a href="mailto:connect@centreforei.com">connect@centreforei.com</a></p>
+<p>London, February 2020</p>
       </div>
     </div>
   )
