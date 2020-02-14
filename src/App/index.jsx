cc6e828b28cc302eaea0403e7d737bffdae4ab85
Philip Fol <philip.fol08@gmail.com>
2020-02-14 00:17:39 +0000

    Add homepage

---
+++ src/App/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css"
+
+const App = props => {
+  return(
+    <div>
+      <div className='header'>Centre for Emotional Intelligence</div>
+      <div className='about'>
+<p>We believe that a basic practical knowledge of Emotional Intelligence (EI) is essential for individual happiness, outstanding teamwork, and fundamentally, a fairer society.</p>
+<p>Our goals are to:
+<ul>
+  <li>organise accessible classes on EI in London led by professionals who are passionate about understanding human emotions</li>
+  <li>provide training workshops for businesses that want to improve the EI skills of their employees</li>
+  <li>build a tested, structured, and teachable curriculum of EI technique</li>
+  <li>connect with people/organisations who see the global importance and value of EI education</li>
+</ul></p>
+<p>We are looking for like-minded people. If you are one of them, get in touch at <a href="mailto:connect@centreforei.com">connect@centreforei.com</a></p>
+<p>London, Feburary 2020</p>
+      </div>
+    </div>
+  )
+}
+
+export default App;
